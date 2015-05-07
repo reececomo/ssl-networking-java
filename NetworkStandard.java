75c3005f1fbd0e065631a126ea7329bb211d3ad1
Reece Como <reece.notargiacomo@gmail.com>
2015-05-07 17:36:38 +0800

    Create NetworkStandard.java

---
+++ NetworkStandard.java
@@ -0,0 +1,15 @@
+/**
+ *  A static class that provides standardised methods for
+ *  interacting between the different clients.
+ */
+ 
+public class NetworkStandard {
+
+  public static boolean syncNonVolatileMemory(String filename, Object[] array) throws IOException {
+    // Take an object array
+    // Convert to a list of strings
+    // Save to file
+    // Check for IOException
+  }
+  
+}
