d4ea7d285e3c0de5225b3949787b6efd45ae78b0
Reece Como <reece.notargiacomo@gmail.com>
2015-05-07 23:00:31 +0800

    Update Message.java

--- lib/Message.java
+++ lib/Message.java
@@ -1,3 +1,5 @@
+package lib;
+
 /**
  *  A standard message class that auto-sets the message on
  *  instantiation, as well as providing a getter for the "flag"
@@ -5,20 +7,29 @@
  */
  
 public class Message {
+	
     private String flag; // flag cannot be changed
     public String content; // contents can be referenced Message.content
-
-  public Message(String rawMessage) {
+    
+  public Message(String rawMessage){
       if(rawMessage.length() <= 4)
-          throw new Exception ("Invalid string length!");
+          this.flag = "NONE";
       else {
-          this.flag = rawMessage.subString(0,4);
-          this.content = rawMessage.subString(4);
+          this.flag = rawMessage.substring(0,4);
+          this.content = rawMessage.substring(4);
       }
   }
   
+  public Message(String flag, String message) {
+	  this(flag+message);
+  }
+ 
   public String getFlag() {
       return this.flag;
+  }
+  
+  public String asData() {
+	  return this.flag + ":" + this.content;
   }
   
 }
