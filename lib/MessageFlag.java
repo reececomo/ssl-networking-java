c738d5e048ea3f960884482dcfda0700550d5b36
Reece Como <reece.notargiacomo@gmail.com>
2015-05-08 18:26:41 +0800

    Delete MessageFlag.java

--- lib/MessageFlag.java
+++
@@ -1,8 +0,0 @@
-package lib;
-
-public class MessageFlag {
-	public static final String BANK_REQ = "REQ";
-	public static final String BANK_WIT = "WITH";
-	public static final String DIR_INIT = "INIT";
-	public static final String EXAM_REQ = "DOIT";
-}
