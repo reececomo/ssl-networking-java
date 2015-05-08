b51d8de95eb37d4ffdf44412f0c26795eb92ad03
Reece Como <reece.notargiacomo@gmail.com>
2015-05-08 19:47:34 +0800

    Update Director.java

--- Director.java
+++ Director.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,71 +16,90 @@ import lib.*;
  * 
  * @date May 8th 2015
  * @author Reece Notargiacomo
+ * @author ...
  *
  */
 public class Director {
-	
+
 	private static final int PORT = 9998;
 	private SSLServerSocket director;
-	
+	private ArrayList<DirectorClient> clients;
 	private boolean socketIsListening = true;
-	
+
 	// main
 	public static void main(String[] args) {
-		Director myDir = new Director( PORT );
+		Director myDir = new Director(PORT);
 	}
-	
-	//constructor
-	public Director ( int portNo ) {
-		
+
+	// constructor
+	public Director(int portNo) {
+
 		// SSL Certificate
-		SSLHandler.declareServerCert("cits3002_01Keystore","cits3002");
+		SSLHandler.declareServerCert("cits3002_01Keystore", "cits3002");
 		ExecutorService executorService = Executors.newFixedThreadPool(100);
 		
+		clients = new ArrayList<DirectorClient>();
+
 		// Start Server and listen
-		if( this.startSocket(portNo) ) 
-			while(this.socketIsListening)
-				
-			try {
-			
-			SSLSocket clientSocket = (SSLSocket) director.accept();
-			executorService.execute(new DirectorClient ( clientSocket ));
-			System.out.println("New client");
-			
-			} catch (IOException err) {
-				
-				System.err.println("Error connecting client " + err);
-			}
-		
+		if (this.startSocket(portNo))
+			while (this.socketIsListening)
+
+				try {
+
+					SSLSocket clientSocket = (SSLSocket) director.accept();
+					System.out.println("Connecting...");
+					DirectorClient newClient = new DirectorClient(clientSocket);
+					clients.add(newClient);
+					executorService.execute(newClient);
+					
+					Thread.sleep(1000);
+					
+					// Test access to analysts/collectors "writer" methods
+					for(DirectorClient client : clients ) {
+						System.out.println(" > Client" + client);
+						Thread.sleep(1000);
+						client.writer.println("SUPERSPRINT\n");
+						Thread.sleep(1000);
+					}
+					
+				} catch (IOException err) {
+
+					System.err.println("Error connecting client " + err);
+					
+				} catch (InterruptedException e) {
+					
+					e.printStackTrace();
+				}
+
 	}
-	
-	public boolean startSocket( int portNo ) {
+
+	public boolean startSocket(int portNo) {
 		try {
 			SSLServerSocketFactory sf;
-			
+
 			sf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-			director = (SSLServerSocket) sf.createServerSocket( portNo );
-			
-			System.out.println("Director started on " + getIPAddress() + ":" + portNo );
-			
+			director = (SSLServerSocket) sf.createServerSocket(portNo);
+
+			System.out.println("Director started on " + getIPAddress() + ":"
+					+ portNo);
+
 			return true;
-			
+
 		} catch (Exception error) {
-			System.err.println("Director failed to start: " + error );
+			System.err.println("Director failed to start: " + error);
 			System.exit(-1);
-			
+
 			return false;
 		}
 	}
 
-	
 	public String getIPAddress() {
 		try {
-			//Get IP Address
+			// Get IP Address
 			return InetAddress.getLocalHost().getHostAddress();
 		} catch (UnknownHostException e) {
 			return "UnknownHost";
 		}
 	}
-	
+
 }
